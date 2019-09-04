0efa1272efb39944b5b066de5d2c95259ea68d32
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-04 02:59:17 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,8 +5,7 @@ class Person
     :weight, :handed, :complexion, :t_shirt_size, 
     :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(:name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
-    :wrist_size, :glove_size, :pant_length, :pant_width)
+  def initialize(:name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width)
     
     @name = name 
     @birthday = birthday
@@ -21,4 +20,5 @@ class Person
     @pant_length = pant_length
     @pant_width = pant_width
   end
+  
 end
