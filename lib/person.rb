c95b9ea47444a49d906a5a4d31f856a4de7b3483
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-04 03:06:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,12 +5,9 @@ class Person
     :weight, :handed, :complexion, :t_shirt_size, 
     :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(attributes)
     def initialize(attributes)
     attributes.each do |key, value|
       self.send(("#{key}="), value)
     end
   end
-  end
-  
 end
